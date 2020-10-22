82e7de3390c740d7cee66dd4ca3f99875401d36e
Alex <alexander.siguenza@gmail.com>
2020-10-22 03:16:54 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Link, Switch }
+  from 'react-router-dom';
+
+import Home from "./components/Home"
+import Info from "./components/Info"
+import Contacto from "./components/Contacto"
+import PageNotFound from "./components/PageNotFound"
+import Help from "./components/Help"
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  // destino (path) 
+  // propiedad exact 
+
+  return (  // JSX  1<div> 2<div></div> </div>
+    // fragment < > </>   
+    <React.Fragment>
+      <Router>
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="/">WebSiteName</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><Link to="/">Inicio</Link></li>
+              <li><Link to="/info">Info</Link></li>
+              <li><Link to="/contacto">Contactanos</Link></li>
+              <li><Link to="/help">Help</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/info" component={Info} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route exact path="/help" component={Help} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
+
 export default App;
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home = () => (
+  <React.Fragment>
+    <p>Bienvenido a Alexs, somos una compañia de desarrollo de software</p>
+  </React.Fragment>
+)
+export default Home
